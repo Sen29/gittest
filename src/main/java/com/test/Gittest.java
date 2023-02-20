6a08011386e5065cb5245267830bf291ca827549
Larry <liusen19930321@outlook.com>
2023-02-20 01:08:37 +0000

    third commit

--- src/main/java/com/test/Gittest.java
+++ src/main/java/com/test/Gittest.java
@@ -6,6 +6,7 @@ package com.test;
 public class Gittest {
     public static void main(String[] args){
         System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
